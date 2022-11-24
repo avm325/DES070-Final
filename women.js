070cde0732fab314a6768afce4309f38d0998126
Aviela M <112045230+avm325@users.noreply.github.com>
2022-11-23 21:39:44 -0500

    n

--- women.js
+++ women.js
@@ -12,3 +12,8 @@ const observer = new IntersectionObserver ((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+
+
